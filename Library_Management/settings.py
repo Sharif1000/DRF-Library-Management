0391d9ccbe4d831f3b35ad6a9d91d02da55f236b
Sharif1000 <sharif.dupharmacy1@gmail.com>
2024-04-28 20:43:52 +0600

    third

--- Library_Management/settings.py
+++ Library_Management/settings.py
@@ -44,12 +44,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'books',
     'account',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
